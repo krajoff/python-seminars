9334ed85d011f5672439e870ec27c12b54731aed
krajoff <iioz@ya.ru>
2022-12-11 13:55:26 +0300

    solved four tasks

--- lesson_6/main.py
+++ lesson_6/main.py
@@ -1,3 +1,6 @@
+import math
+import pandas as pd
+
 # 5. Дан список интов, повторяющихся элементов в списке нет. Нужно преобразовать это
 # множество в строку, сворачивая соседние по числовому ряду числа в диапазоны.
 # Примеры: [1,4,5,2,3,9,8,11,0] => "0-5,8-9,11",  [1,4,3,2] => "1-4", [1,4] => "1,4"
@@ -52,5 +55,44 @@ def compress(instr: str):
         else:
             res += str(cnt) + instr[idx]
         return res
+
+
 print('Задача 2:\nВходная строка: ' f'{string}')
 print('Результат: ' + compress(string) + '\n')
+
+
+# 7. Планеты вращаются вокруг звезд по эллиптическим орбитам. Назовём самой далёкой планетой ту,
+# орбита которой имеет самую большую площадь. Напишите функцию find_farthest_orbit(list_of_orbits),
+# которая среди списка орбит планет найдет ту, по которой вращается самая далёкая планета. Круговые
+# орбиты не учитывайте: вы знаете, что у вашей звезды таких планет нет, зато искусственные спутники
+# были запущены на круговые орбиты. Результатом функции должен быть кортеж, содержащий длины полуосей
+# эллипса орбиты самой далёкой планеты. Каждая орбита представляет из себя кортеж из пары чисел –
+# полуосей её эллипса. Площадь эллипса вычисляется по формуле S = πab, где a и b – длины полуосей эллипса.
+# При решении задачи используйте списочные выражения. Подсказка: проще всего будет найти эллипс в два шага:
+# сначала вычислить самую большую площадь эллипса, а затем найти и сам эллипс, имеющий такую площадь.
+# Гарантируется, что самая далёкая планета ровно одна. Ввод orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+# Вывод 2.5 10
+
+
+def find_farthest_orbit(list_of_orbits):
+    filtered_list = list(filter(lambda x: x[0] != x[1], list_of_orbits))
+    list_square = [math.pi * orbit[0] * orbit[1] for orbit in filtered_list]
+    max_orbit = list_square.index(max(list_square))
+    return filtered_list[max_orbit]
+
+
+orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+print('Задача 3:\nВходная строка: ' f'{orbits}')
+print('Результат: ' + f'{find_farthest_orbit(orbits)}' + '\n')
+
+
+def print_operation_table(operation, num_rows, num_columns):
+    table = []
+    for k in range(1, num_rows):
+        row = list(map(operation, [x for x in range(1, num_columns)], [k for _ in range(1, num_columns)]))
+        table.append(row)
+    print(pd.DataFrame(table, columns=[i for i in range(1, num_columns)], index=[i for i in range(1, num_rows)]))
+
+
+print('Задача 4')
+print_operation_table(lambda x, y: x * y, 10, 10)
